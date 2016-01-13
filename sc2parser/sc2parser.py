61b9057dcc961062b1c2dca348446f24241c2f6e
Patrick <patrick.brand2@gmail.com>
2016-01-13 23:14:41 +0100

    Changed playerId to userId for economy data.

--- sc2parser/sc2parser.py
+++ sc2parser/sc2parser.py
@@ -37,6 +37,8 @@ class Sc2ReplayParser:
         output_list['baseBuild'] = self.__reader.get_replay_protocol_version()
         output_list['mapName'] = replay_details['m_title']
         output_list['gameStart'] = (replay_details['m_timeUTC'] / 10000000) - 11644473600 # Found at https://github.com/karlgluck/heroes-of-the-storm-replay-parser
+        last_registered_game_loop = self.__reader.get_replay_tracker_events()[-1]['_gameloop'] 
+        output_list['gameTime'] = last_registered_game_loop / 16.0 # in seconds and corresponds with ggtracker
         output_list['isBlizzardMap'] = replay_details['m_isBlizzardMap']
         output_list['gameSpeed'] = replay_details['m_gameSpeed']
         parsed_player_list = []
@@ -60,12 +62,20 @@ class Sc2ReplayParser:
         output_list['playerList'] = parsed_player_list
     
     def __parse_economy_data(self, output_list):
-        for event in self.__reader.get_replay_tracker_events():
+        # PlayerId -> UserId mapping
+        playerIdToUserId = {}
+        tracker_events = self.__reader.get_replay_tracker_events()
+        for event in tracker_events:
+            if event['_event'] == 'NNet.Replay.Tracker.SPlayerSetupEvent':
+                playerIdToUserId[event['m_playerId']] = event['m_userId']
+        
+        for event in tracker_events:
             if event['_eventid'] == 0: # NNet.Replay.Tracker.SPlayerStatsEvent
                 economy_entry = {}
                 economy_entry['gameloop'] = event['_gameloop']
-                economy_entry['playerId'] = event['m_playerId'] # WIP add events under each playerId, but don't know how to link all playerId's yet.
-                # capute all stats
+                #economy_entry['playerId'] = event['m_playerId'] # WIP add events under each playerId, but don't know how to link all playerId's yet.
+                economy_entry['userId'] = playerIdToUserId[event['m_playerId']]
+                # capture all stats
                 for stat in event['m_stats'].keys():
                     economy_entry[string.strip(stat,'m_')] = event['m_stats'][stat]
                 output_list.append(economy_entry)
